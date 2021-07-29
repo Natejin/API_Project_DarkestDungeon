573288eb3814a76cf306c20911a40609f1613d01
MJ5353 <88023049+MJ5353@users.noreply.github.com>
2021-07-29 16:20:15 +0900

    210729_MJ_enemy
    ++

--- CEnemy.h
+++ CEnemy.h
@@ -6,6 +6,7 @@ class CEnemy : public Unit
 {
 protected:
 	RACE m_race;
+<<<<<<< Updated upstream
 	int HP;
 	int DODGE;
 	int DEF;
@@ -23,6 +24,17 @@ protected:
 	//스킬 Info에 accuracy, critical, DMG, changeAttribute 가 포함됨
 	int Skill[4]; //1, 2, 3, 4
 	
+=======
+	int m_Hp;
+	int m_Dodge;
+	int m_DMG; //스킬에서 값을 변경하여 사용
+	int m_DEF;
+	int m_SPD;
+
+	int resist[5]; //Stun, Blight, Move, Bleed, Debuff
+	int attribute[6]; //Stun, Blight, Move, Bleed, Debuff, Buff, marked
+
+>>>>>>> Stashed changes
 
 public:
 	CEnemy();
