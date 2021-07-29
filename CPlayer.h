7393cc753588bca71a9fac1617dc718353144f12
wootak1995 <85159317+wootak1995@users.noreply.github.com>
2021-07-29 15:53:24 +0900

    210729_WT_Cplayer특성추가
    영웅들이 가지고있을거 같은 특성들 추가

--- CPlayer.h
+++ CPlayer.h
@@ -6,13 +6,39 @@ class CPlayer : public Unit
 protected:
 	JOB job;
 	int stress;
+	int Hp;
+	int Def;
+	int Speed;
+	int Damage;
+	int Critical;
+	int Accuracy; //명중률
+	int Evasion;
+	int Exp;
+	//장비나 장신구는 어떻게?
 
+	enum class Resistance
+	{
+		Stun,
+		Blight,
+		Bleed,
+		Debuff,
+		Move,
+		Death_Door
+	};
+	enum class Combat_Skill
+	{
 
-
-
-
-
-
+	};
+	enum class Camping_Skill
+	{	//야습방지
+		Night_Attack_Prevention,
+		//정찰 강화
+		Reconnaissance,
+		Stress_Down,
+		Cure_disease,
+		Cure_Death_Door,
+		Cure_Hp
+	};
 
 public:
 	CPlayer();
