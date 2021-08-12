2c115cfe9ac9899a6fa9c5249af246949842f7c5
Natejin <38517532+OneThingChanged@users.noreply.github.com>
2021-08-12 19:50:32 +0900

    12

--- CHeroList_button.h
+++ CHeroList_button.h
@@ -21,19 +21,6 @@ public:
 	virtual void Render(HDC _hdc);
 	virtual void FrontRender(HDC _hdc);
 
-<<<<<<< HEAD
-	//void SetButtonSize(float width, float height);
-
-	//template <typename T>
-	//void SetTriggerWhenDrag(T* pObj, void(T::* func)())
-	//{
-	//	canTriggerDrag = true;
-	//	m_triggerWhenDrag = bind(func, pObj);
-	//}
-=======
-	void DragHeroList();
-
->>>>>>> parent of 9433e27 (2021.08.12)
 
 	void DragHeroList();
 
