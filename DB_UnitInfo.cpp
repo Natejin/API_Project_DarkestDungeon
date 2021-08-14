86c98f48992dd21c34a729ce9d1a3569a7106f7c
Natejin <38517532+OneThingChanged@users.noreply.github.com>
2021-08-14 19:00:24 +0900

    210814_JT

--- DB_UnitInfo.cpp
+++ DB_UnitInfo.cpp
@@ -1,2 +1,29 @@
 #include "framework.h"
 #include "DB_UnitInfo.h"
+
+DB_UnitInfo::DB_UnitInfo()
+{
+}
+
+DB_UnitInfo::~DB_UnitInfo()
+{
+}
+
+void DB_UnitInfo::Init()
+{
+
+
+}
+
+CInfo_Item* DB_UnitInfo::CallItem(ITEM item)
+{
+	return nullptr;
+}
+
+void DB_UnitInfo::Save()
+{
+}
+
+void DB_UnitInfo::Load()
+{
+}
--- DB_UnitInfo.h
+++ DB_UnitInfo.h
@@ -1,5 +1,23 @@
 #pragma once
-class DB_UnitInfo
-{
-};
+#include "Info_Enemy.h"
+#include "Info_Hero.h"
 
+class DB_UnitInfo : public Singleton<DB_UnitInfo>
+{
+private:
+	map<JOB, Info_Hero*> heroDB;
+	map<JOB, Info_Enemy*> enemyDB;
+	int heroID;
+	int enemyID;
+
+public:
+	DB_UnitInfo();
+	~DB_UnitInfo();
+
+	void Init();
+
+	CInfo_Item* CallItem(ITEM item);
+
+	void Save();
+	void Load();
+};
