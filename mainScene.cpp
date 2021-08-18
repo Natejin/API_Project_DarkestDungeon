c599aec7a538f49d522837b56a45b1a618c8ef94
wootak1995 <85159317+wootak1995@users.noreply.github.com>
2021-08-18 22:29:38 +0900

    walking as

---
+++ mainScene.cpp
@@ -0,0 +1,31 @@
+#include "framework.h"
+#include "mainScene.h"
+
+mainScene::mainScene()
+{
+}
+
+mainScene::~mainScene()
+{
+}
+
+HRESULT mainScene::Init()
+{   
+    return S_OK;
+}
+
+void mainScene::Release()
+{
+}
+
+void mainScene::Update()
+{
+}
+
+void mainScene::Render()
+{
+}
+
+void mainScene::Render(HDC _hdc)
+{
+}
---
+++ mainScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"Scene.h"
+
+class mainScene :public Scene
+{
+public:
+	mainScene();
+	~mainScene();
+
+	virtual HRESULT Init();
+	virtual void Release();
+	virtual void Update();
+	virtual void Render();
+	virtual void Render(HDC _hdc);
+
+};
+
