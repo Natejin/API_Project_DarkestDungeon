948f3c5bab8128fce6bb4903d2236057f4e0ebbe
Natejin <38517532+OneThingChanged@users.noreply.github.com>
2021-08-19 10:02:04 +0900

    fix

--- dungeonUI_HeroInfo.cpp
+++ dungeonUI_HeroInfo.cpp
@@ -126,25 +126,26 @@ void CDungeonUI_HeroInfo::ShowHeroState(HDC _hdc)
             break;
         }
         TextOut(_hdc, 450, 780, str, strlen(str));
+        SetTextColor(_hdc, RGB(255, 255, 255));
+        sprintf_s(str, " %d / 200", MG_GAME->GetHeroFromParty(selectedHeroIndex)->getStress());
+        TextOut(_hdc, 400, 860, str, strlen(str));
+
+        sprintf_s(str, "ACC         %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetAcry());
+        TextOut(_hdc, 380, 885, str, strlen(str));
+        sprintf_s(str, "CRIT        %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetCri());
+        TextOut(_hdc, 380, 905, str, strlen(str));
+        sprintf_s(str, "DMG       %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDmg());
+        TextOut(_hdc, 380, 925, str, strlen(str));
+        sprintf_s(str, "DODGE   %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDodge());
+        TextOut(_hdc, 380, 945, str, strlen(str));
+        sprintf_s(str, "PROT      %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDef());
+        TextOut(_hdc, 380, 965, str, strlen(str));
+        sprintf_s(str, "SPD        %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->getSPD());
+        TextOut(_hdc, 380, 985, str, strlen(str));
     }
   
 
-    SetTextColor(_hdc, RGB(255, 255, 255));
-    sprintf_s(str, " %d / 200", MG_GAME->GetHeroFromParty(selectedHeroIndex)->getStress());
-    TextOut(_hdc, 400, 860, str, strlen(str));
 
-    sprintf_s(str, "ACC         %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetAcry());
-    TextOut(_hdc, 380, 885, str, strlen(str));
-    sprintf_s(str, "CRIT        %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetCri());
-    TextOut(_hdc, 380, 905, str, strlen(str));
-    sprintf_s(str, "DMG       %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDmg());
-    TextOut(_hdc, 380, 925, str, strlen(str));
-    sprintf_s(str, "DODGE   %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDodge());
-    TextOut(_hdc, 380, 945, str, strlen(str));
-    sprintf_s(str, "PROT      %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->GetDef());
-    TextOut(_hdc, 380, 965, str, strlen(str));
-    sprintf_s(str, "SPD        %d", MG_GAME->GetHeroFromParty(selectedHeroIndex)->getSPD());
-    TextOut(_hdc, 380, 985, str, strlen(str));
 }
 
 void CDungeonUI_HeroInfo::showHeroSkill(HDC _hdc)
